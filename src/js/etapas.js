ad109aa20da4e965c6d871aa36b49113d1f691e9
Alessandro Júnior <80294932+alvesalejr@users.noreply.github.com>
2022-08-16 18:05:36 -0300

    Add files via upload

---
+++ src/js/etapas.js
@@ -0,0 +1,49 @@
+// Foi definido um ARRAY para Etapas;
+let etapas = [
+    {
+        titulo: 'DEPUTADO',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '88888',
+                nome: 'Fulano de Tal',
+                partido: "ABC",
+                fotos: [
+                    { url: 'homer.jpg', legenda: 'Deputado', small: false }
+                ]
+            },
+            {
+                numero: '55555',
+                nome: 'Beltrano de Tal',
+                partido: 'GAMA',
+                fotos: [{ url: 'lisa.jpg', legenda: 'Deputada' }
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclado',
+                partido: 'ABC',
+                vice: 'Cicl',
+                fotos: [{ url: 'trump.jpg', legenda: 'Presidente' },
+                { url: 'lisa.jpg', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+            {
+                numero: '10',
+                nome: 'Zulano',
+                partido: 'CLAN',
+                vice: 'Ciclano',
+                fotos: [{ url: 'trump.jpg', legenda: 'Presidente' },
+                { url: 'lisa.jpg', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+        ]
+    },
+];
